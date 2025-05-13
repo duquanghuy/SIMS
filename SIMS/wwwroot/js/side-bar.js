5293798e96342f9c912d69b128bb772dff338c55
Quang Huy <huy.du@sotatek.com>
2025-05-13 14:33:40 +0700

    update page layout

---
+++ SIMS/wwwroot/js/side-bar.js
@@ -0,0 +1,7 @@
+﻿document
+    .getElementById("sidebarToggle")
+    .addEventListener("click", function () {
+        var sb = document.getElementById("sidebarContent");
+        var main = document.getElementById("mainContent");
+        sb.classList.toggle("collapsed");
+    });
